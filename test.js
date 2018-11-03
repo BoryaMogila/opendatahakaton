9e6e6e4e054a875580b5cbc1ee21697af51c2ea3
alex <a.chepugov@gmail.com>
2018-11-03 14:48:57 +0200

    points

--- test.js
+++ test.js
@@ -13,20 +13,33 @@ async function run() {
   let img = new Image();
   img.src = fs.readFileSync('./2018-11-03.jpg');;
   const canvas = createCanvas(img.width,img.height);
+
   const ctx = canvas.getContext('2d');
   ctx.drawImage(img,0,0);
+    console.log('test.js: 19')
 
-  const imageScaleFactor = 0.5;
+    const imageScaleFactor = 0.5;
   const flipHorizontal = false;
   const outputStride = 8;
   const multiplier = 0.5;
 
   const net  = await posenet.load(multiplier);
-  const pose = await net.estimateSinglePose(canvas, imageScaleFactor, flipHorizontal, outputStride);
+  const poses = await net.estimateMultiplePoses(canvas, imageScaleFactor, flipHorizontal, outputStride);
   ctx.fillText('Awesome!', 50, 100);
-  console.log(pose, canvas);
+  // console.log(poses, canvas);
+
+
+ for(let pose of poses ) {
+     let {keypoints} = pose;
+     keypoints.forEach(({position: {x, y } = {}} = {}) => {
+         console.log('test.js: 36', x, y);
+         ctx.fillRect(x,y,5,5)
+     })
+
+ }
+
   fs.writeFileSync('out.jpg', canvas.toBuffer())
-  return pose;
+  // return pose;
 }
 
 run();
