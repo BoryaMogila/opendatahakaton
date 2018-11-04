e1c4f501651b752470bc8fcc876539603171773e
alex <a.chepugov@gmail.com>
2018-11-04 13:48:43 +0200

    demo

--- demo/demo.js
+++ demo/demo.js
@@ -1,15 +1,16 @@
-const app = require('express')(),
-    demo = require('http').Server(app),
-    io = require('socket.io')(demo),
+const app = require('express')();
+const demo = require('http').Server(app);
+const io = require('socket.io')(demo);
+
 demo.listen(8082);
 
-io.on('connection', function(socket) {
+io.on('connection', function (socket) {
     console.log('connection');
 
-    socket.on('disconnect', function() {
+    socket.on('disconnect', function () {
         // stream.removeListener('data', pipeStream);
     });
-    socket.on('image',  function(data) {
+    socket.on('image', function (data) {
         console.log("image", data);
         socket.emit('data', data);
     });
